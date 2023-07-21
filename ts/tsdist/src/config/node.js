84ff6a5526d9a571f4aa621d58b01fcb15133a7d
imteammy <kartoonn868@gmail.com>
2023-07-21 16:04:53 +0700

    del

--- ts/tsdist/src/config/node.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_cache_1 = __importDefault(require("node-cache"));
-const NodeCache = new node_cache_1.default();
-exports.default = NodeCache;
